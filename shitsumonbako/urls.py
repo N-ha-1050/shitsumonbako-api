d2c9043a71cb8b5b9dd12a04a86b7a4a7f63f061
N_ha <naganoharuho@gmail.com>
2021-09-14 08:21:40 +0900

    change to apiview

--- shitsumonbako/urls.py
+++ shitsumonbako/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from shitsumonbako import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
     path('questions/', views.question_list),
     path('questions/<int:pk>/', views.question_detail),
 ]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
